ea7dcf208b3449fca7acc6926363d9567bfa18e5
Hans <hans.huang@gus-tech.com.tw>
2025-12-11 16:27:45 +0800

    feat(greeting): 新增遙控車與賽道的結構定義及相關功能

---
+++ greeting/need_for_speed.go
@@ -0,0 +1,56 @@
+package greeting
+
+// TODO: define the 'Car' type struct
+type Car struct {
+	battery      int
+	batteryDrain int
+	speed        int
+	distance     int
+}
+
+type Track struct {
+	distance int
+}
+
+// NewCar creates a new remote controlled car with full battery and given specifications.
+func NewCar(speed, batteryDrain int) Car {
+	return Car{
+		battery:      100,
+		distance:     0,
+		speed:        speed,
+		batteryDrain: batteryDrain,
+	}
+}
+
+// TODO: define the 'Track' type struct
+
+// NewTrack creates a new track
+func NewTrack(distance int) Track {
+	return Track{
+		distance: distance,
+	}
+}
+
+// Drive drives the car one time. If there is not enough battery to drive one more time,
+// the car will not move.
+func Drive(car Car) Car {
+	if car.battery >= car.batteryDrain {
+		car.distance += car.speed
+		car.battery -= car.batteryDrain
+	}
+
+	return car
+}
+
+// CanFinish checks if a car is able to finish a certain track.
+func CanFinish(car Car, track Track) bool {
+	count := 0
+	if track.distance%car.speed == 0 {
+		count = track.distance / car.speed
+	} else {
+		count = (track.distance / car.speed) + 1
+	}
+
+	totalDrain := count * car.batteryDrain
+	return totalDrain <= car.battery
+}
