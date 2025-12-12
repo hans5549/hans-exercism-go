665247dd50f08bbcad2c59c2febde2436f350bf7
Hans <hans.huang@gus-tech.com.tw>
2025-12-12 11:50:23 +0800

    refactor(greeting): 將PreparationTime函數重命名為CalculatePreparationTime

--- greeting/lasagna.go
+++ greeting/lasagna.go
@@ -7,12 +7,12 @@ func RemainingOvenTime(actualMinutesInOven int) int {
 	return OvenTime - actualMinutesInOven
 }
 
-// PreparationTime calculates the time needed to prepare the lasagna based on the amount of layers.
-func PreparationTime(numberOfLayers int) int {
+// CalculatePreparationTime calculates the time needed to prepare the lasagna based on the amount of layers.
+func CalculatePreparationTime(numberOfLayers int) int {
 	return numberOfLayers * 2
 }
 
 // ElapsedTime calculates the time elapsed cooking the lasagna. This time includes the preparation time and the time the lasagna is baking in the oven.
 func ElapsedTime(numberOfLayers, actualMinutesInOven int) int {
-	return PreparationTime(numberOfLayers) + actualMinutesInOven
+	return CalculatePreparationTime(numberOfLayers) + actualMinutesInOven
 }
